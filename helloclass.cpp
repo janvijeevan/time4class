74e1cb4976bee4e036d4f44d93f3710728997186
janvi jeevan <18ec012@mgits.ac.in>
2021-06-20 18:23:10 +0530

    first program

---
+++ helloclass.cpp
@@ -0,0 +1,6 @@
+public class helloclass {
+   public static void main (string[] args) {
+       // prints "hello, class" to the terminal window.
+       system.out,println("hello, class");
+      }
+  }
